c4ffb4efbfc3c547a08949d621c6fa958cdd6fc9
Donte Harris <harrisdonte@hotmail.com>
2021-04-20 22:11:41 -0400

    added the paths the players can chose and take when playing this mud

--- game-api/my-app/src/pusher.js
+++
@@ -1,9 +0,0 @@
-import Pusher from 'pusher-js';
-
-const socket = new Pusher('PUSHER_KEY', {
-    cluster: 'PUSHER_CLUSTER',
-    encrypted: true,
-    authEndpoint: 'http://localhost:4000/pusher/auth'
-});
-
-export default socket;
